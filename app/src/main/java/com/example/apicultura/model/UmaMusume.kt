05d786a925649affefb2be7b49245cf1c4e8ae95
Jordi Fernández Arlegui <jordi.fernandez@gracia.lasalle.cat>
2026-01-11 15:48:26 +0100

    model UmaMusume y carpetas

---
+++ app/src/main/java/com/example/apicultura/model/UmaMusume.kt
@@ -0,0 +1,9 @@
+package com.example.apicultura.model
+
+data class UmaMusume(
+    val id: Int,
+    val name: String,
+    val imageUrl: String,
+    val rarity: String,
+    val description: String
+)
